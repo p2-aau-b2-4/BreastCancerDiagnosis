912792a8213b19eeef8dcd734bc2bcf483420496
b2-4 <b2-4@b2-4.zyxel.com>
2019-03-07 12:54:29 +0000

    Rendering works

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using Dicom;
+using Dicom.Imaging;
+
+namespace DicomDisplayTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ImageManager.SetImplementation(WinFormsImageManager.Instance);
+      var image = new DicomImage(@"e.dcm");
+      image.RenderImage().AsClonedBitmap().Save(@"test1.bmp");
+    }
+  }
+}
