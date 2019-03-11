f2bc866bea61d40c58103beafca479504cd7a0c7
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-03-11 11:08:08 +0100

    Added extension functions for getting and setting the pixel data of a dcm file
    Signed-off-by: Julius Nyborg Olesen <hoolius.o@gmail.com>

---
+++ DicomFileExtensions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Text;
+using System.Windows;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using Dicom;
+using Dicom.IO.Buffer;
+using Dicom.Imaging;
+
+namespace DicomDisplayTest
+{
+  public static class DicomFileExtensions
+  {
+    public static byte[,] GetByteMatrix(this DicomFile f)
+    {
+      DicomPixelData pixelData = DicomPixelData.Create(f.Dataset);
+
+      byte[] byteData = pixelData.GetFrame(0).Data;
+
+      int columns = f.Dataset.Get<int>(DicomTag.Columns);
+      int rows = f.Dataset.Get<int>(DicomTag.Rows);
+
+      byte[,] byteMatrix = new byte[columns, rows];
+
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++)
+          byteMatrix[j, i] = byteData[(rows * i) + j];
+      }
+      return byteMatrix;
+    }
+    public static void SetByteMatrix(this DicomFile f, byte[,] byteMatrix)
+    {
+      int columns = byteMatrix.GetLength(0);
+      int rows = byteMatrix.GetLength(1);
+
+      byte[] byteArray = new byte[columns+rows];
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+          byteArray[(i * rows) + columns] = byteMatrix[i, j];
+        }
+      }
+      MemoryByteBuffer byteBuffer = new MemoryByteBuffer(byteArray);
+
+      f.Dataset.AddOrUpdatePixelData(DicomVR.OB, byteBuffer);
+    }
+  }
+}
