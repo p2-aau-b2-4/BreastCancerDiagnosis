338a6bcefcd7e03ab8794faf4bc71c4b8fec79cd
Yogi <markamb2497@gmail.com>
2019-03-14 18:48:06 +0100

    Contrast working method name not correct and method is missing auto threshold detection

--- Contrast.cs
+++ Contrast.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Xml.Linq;
 using Dicom;
 using Dicom.Imaging;
 using Dicom.Imaging.Codec;
@@ -8,73 +9,47 @@ namespace DicomDisplayTest
 {
     public class Contrast
     {
-        public static ushort[,] equalization(ushort[,] image)
-        //public static Bitmap equalization(ushort[,] image)
+        public static ushort[,] Equalization(ushort[,] image, double threshold)
         {
             
             // Copying image to int array 
             //int[] pixels = new int[image2.Height * image2.Width];
-            ushort[,] new_pixels = new ushort[image.GetLength(0), image.GetLength(1)];
-            
+            ushort[,] pixels = new ushort[image.GetLength(0), image.GetLength(1)];
+            ushort[,] end_pixels = new ushort[image.GetLength(0), image.GetLength(1)];
             
             for (int y = 0; y < image.GetLength(1); y++)
             {
                 for (int x = 0; x < image.GetLength(0); x++)
                 {
-                    new_pixels[x,y] = image[x,y];
-                }
-            }
-            
-            ushort[] cdf = new ushort[256];
-            ushort val;
-            for (int y = 0; y < image.GetLength(1); y++)
-            {
-                for (int x = 0; x < image.GetLength(0); x++)
-                {
-                    val = new_pixels[x,y];
-                    cdf[val]++;
-                }
-            }
-            
-            Console.WriteLine("waypoint");
-            // Finding the minimum brightness
-            
-            
-            ushort cdfMin = cdf[0];
-            for(int i = 1; i < 256; i++) {
-                cdf[i] += cdf[i-1];
-                if(cdf[i] < cdfMin) {
-                    cdfMin = cdf[i];
-                }
-            }
-            
-            // Change pixels array
-            /*for(int y = 0; y < image2.Height; y++) {
-                for(int x = 0; x < image2.Width; x++) {
-                    new_pixels[image2.Width*y + x] = Convert.ToInt32((Math.Round((double) cdf[pixels[image2.Width*y + x]] - cdf_min)*255.0/(image2.Width*image2.Height-cdf_min)));
-                }
-            }*/
-            
-            for(int y = 0; y < image.GetLength(1); y++) 
-            {
-                for(int x = 0; x < image.GetLength(0); x++)
-                {
-                    new_pixels[x,y] = Convert.ToUInt16((Math.Round((double) cdf[new_pixels[x,y]] - cdfMin)*255.0/(image.GetLength(0)*image.GetLength(1)-cdfMin)));
+                    pixels[x,y] = image[x,y];
                 }
             }
 
-            var bitmap = new Bitmap(image.GetLength(0), image.GetLength(1));
-            for(int y = 0; y < image.GetLength(1); y++) {
-                for(int x = 0; x < image.GetLength(0); x++)
+            //double threshold = 50.0;
+            double contrastLevel = Math.Pow((100.0 + threshold) / 100.0, 2);
+            double grey = 0.0;
+
+            for (int y = 0; y < image.GetLength(1); y++)
+            {
+                for (int x = 0; x < image.GetLength(0); x++)
                 {
-                    bitmap.SetPixel(x,y,Color.FromArgb(new_pixels[x,y],new_pixels[x,y],new_pixels[x,y]));
+                    grey = ((((pixels[x,y] / 65535.0) - 0.5) *  
+                             contrastLevel) + 0.5) * 65535.0;
+                    
+                    if (grey > 65535)
+                    {
+                        grey = 65535;
+                    } 
+                    else if (grey < 0)
+                    {
+                        grey = 0;
+                    }
+
+                    end_pixels[x, y] = Convert.ToUInt16(grey);
                 }
             }
-            
-            // Trying to make a grayscale bitmap insted of RGB
-            //bitmap.SetPixel(x,y, Color.FromArg(new_pixels[x], new_pixels[x], new_pixels[x]));
-            
-            return new_pixels;
+
+            return end_pixels;
         }
     }
 }
