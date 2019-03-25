3798ad3ce81455f0f3b469fbaebedb59c830ec86
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-03-25 14:35:56 +0100

    Added Unit tests for current Vector lib

---
+++ LinearAlgebra.Tests/VectorTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using LinearAlgebra;
+using NUnit.Framework;
+
+namespace LinearAlgebra.Test
+{
+  [TestFixture, Description("Tests for the Vector class")]
+  public class VectorTests
+  {
+
+    [Test, Description("Is the null vector generated when no arguments are passed to Vector")]
+    public void ConstructNullVectorValueIsZero()
+    {
+      Vector nullVector = new Vector();
+      Assert.AreEqual(new List<double>() {0}, nullVector.Elements);
+    }
+
+    [Test, Description("Transposed state can be set to true")] 
+    public void SetTransposedStateTrue()
+    {
+      Vector vector = new Vector(false);
+      vector.Transpose();
+      Assert.AreEqual(true, vector.Transposed);
+    }
+
+    [Test, Description("Transposed state can be set to false")] 
+    public void SetTransposedStateFalse()
+    {
+      Vector vector = new Vector(true);
+      vector.Transpose();
+      Assert.AreEqual(false, vector.Transposed);
+    }
+
+    [Test, Description("Constructor works with 1 value")]
+    public void ConstructSingleDimensionVector()
+    {
+      Vector vector = new Vector(double.MaxValue);
+      Assert.AreEqual(new List<double>() {double.MaxValue}, vector.Elements);
+    }
+
+    [Test, Description("Constructor works with multiple values")]
+    public void ConstructMultiDimensionalVector()
+    {
+      List<double> points = new List<double>() {double.MaxValue, double.MinValue, 0, -1, 1, 5.123, 682.3}
+      Vector vector = new Vector(points);
+      Assert.AreEqual(vector.Elements, points);
+    }
+
+  }
+}
