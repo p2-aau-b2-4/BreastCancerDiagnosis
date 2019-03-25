c6febdcfaf1e01761e27089c11f1e7e6d4b1f066
Yogi <markamb2497@gmail.com>
2019-03-25 12:48:32 +0100

    Panic programming no result

--- Program2.cs
+++ Program2.cs
@@ -6,6 +6,7 @@ using Accord.Math;
 using Accord.Math.Decompositions;
 using Accord.Statistics;
 using Accord.Statistics.Analysis;
+using Accord.Statistics.Kernels;
 using Accord.Statistics.Models.Regression.Linear;
 
 namespace DicomDisplayTest
@@ -16,14 +17,14 @@ namespace DicomDisplayTest
         {
             PixelVector[,] iv = new PixelVector[28,28];
             
-            double[][] dArray = new double[28*28][];
+            double[][] dArray = new double[3370][];
 
             for (int i = 0; i < 3370; i++)
             {
                 dArray[i] = new double[28*28];
             }
 
-            for (var img = 0; img < 1; img++)
+            for (var img = 0; img < 3370; img++)
             {
                 string path = "mnist1/" + img + "_.png";
                 Bitmap bmap = new Bitmap(path);
@@ -32,7 +33,7 @@ namespace DicomDisplayTest
                     for (var x = 0; x < 28; x++)
                     {
                         //iv[x,y] = new PixelVector(x,y,bmap.GetPixel(x, y).R);
-                        dArray[(y*28)+y][img] = (double)bmap.GetPixel(x, y).R;
+                        dArray[img][(y*28)+x] = (double)bmap.GetPixel(x, y).R;
                         //int r = bmap.GetPixel(x, y).R;
                     }
                 }
@@ -72,33 +73,36 @@ namespace DicomDisplayTest
                 { 3, 2, 4, 128, 43, 65, 1, 255, 34, 200, 74, 245, 153, 100, 70, 20, 5, 0, 0, 15, 122, 245, 255, 255, 10, 254, 52, 28 }
             };
             
-            double[][] pcaArray = new double[28*28][];
-            for (int z = 0; z < 1; z++)
+            /*double[][] pcaArray = new double[28*28][];
+            for (int z = 0; z < 33; z++)
             {
                 for (var y = 0; y < 28; y++)
                 {
                     for (var x = 0; x < 28; x++)
                     {
-                        pcaArray[(y * 28) + x] = dArray[(y * 28) + x];
+                        pcaArray[(y * 28) + x] = dArray[z][(y * 28) + x];
                     }
                 }
-            }
+            }*/
             
             PrincipalComponentAnalysis pca = new PrincipalComponentAnalysis();
-            pca.Learn(pcaArray);
+            var pissaf = pca.Learn(dArray);
+            
+
+            var pcaRevert = pca.Revert(pissaf.Transform(dArray));
             
             // Create an Eigenvalue decomposition
-            var evd = new EigenvalueDecomposition(M);
+            //var evd = new EigenvalueDecomposition(M);
             // Store the eigenvalues and eigenvectors
-            double[] λ = evd.RealEigenvalues;
-            double[,] V = evd.Eigenvectors;
+            //double[] λ = evd.RealEigenvalues;
+            //double[,] V = evd.Eigenvectors;
             // Reconstruct M = V*λ*V'
             /*double[,] R =
                 V.MultiplyByDiagonal(λ).MultiplyByTranspose(V);*/
-            double[,] R =
-                V.DotWithDiagonal(λ).DotWithTransposed(V);
+            //double[,] R =
+              //  V.DotWithDiagonal(λ).DotWithTransposed(V);
             // Show on screen
-            Console.WriteLine("Matrix: ");
+            /*Console.WriteLine("Matrix: ");
             Console.WriteLine(dArray.ToString(" 0"));
             Console.WriteLine();
             Console.WriteLine("Eigenvalues: ");
@@ -108,38 +112,44 @@ namespace DicomDisplayTest
             Console.WriteLine(V.ToString(" +0.000; -0.000;"));
             Console.WriteLine();
             Console.WriteLine("Reconstruction:");
-            Console.WriteLine(R.ToString(" 0"));
-
-            int maxVal = 0;
-            for (var x = 0; x < 28; x++)
+            Console.WriteLine(R.ToString(" 0"));*/
+            var piss = pissaf.Weights;
+            double maxVal = 0;
+            for (var y = 0; y < 28; y++)
             {
-                for (var y = 0; y < 28; y++)
+                for (var x = 0; x < 28; x++)
                 {
-                    if ((int) Math.Abs(R[x, y]) > maxVal)
-                        maxVal = (int) Math.Abs(R[x, y]);
+                    if (Math.Abs(piss[0][(y*28)+x]) > maxVal)
+                        maxVal = Math.Abs(piss[0][(y*28)+x]);
                 }
             }
             
             Bitmap Rbmap = new Bitmap(28,28);
-            for (var x = 0; x < 28; x++)
+            for (int img = 0; img < 1; img++)
             {
                 for (var y = 0; y < 28; y++)
                 {
-                    //decimal res = ExtensionMethods.Map((int)R[x, y],0,254,minVal,maxVal);
-                    double res = Math.Abs(R[x, y]);
-                    if (res != 0)
-                        res = 255;
-                    //res = Map((float)res,0,maxVal,0,255);
-                    Rbmap.SetPixel(x, y, Color.FromArgb((int)res,(int)res,(int)res));
-                    R[x, y] = res;
+                    for (var x = 0; x < 28; x++)
+                    {
+                        //decimal res = ExtensionMethods.Map((int)R[x, y],0,255,minVal,maxVal);
+                        
+                        double res = Math.Abs(piss[img][(y * 28) + x]);
+                        /*if (res != 0)
+                            res = 255;*/
+                        res = Map(res,0,maxVal,0,255);
+                        Rbmap.SetPixel(x, y, Color.FromArgb((int)res,(int)res,(int)res));
+                        pcaRevert[img][(y *28) + x] = res;
+                    }
                 }
             }
             
             Console.WriteLine();
             Console.WriteLine("Reconstruction RGB:");
-            Console.WriteLine(R.ToString(" 0"));
+            //Console.WriteLine(pcaRevert.ToString(" 0"));
+            Console.WriteLine(pissaf.Intercepts.ToString(" 0")); 
             
-            Rbmap.Save("Rtest.png",ImageFormat.Png);
+            
+            Rbmap.Save("Rtest.png", ImageFormat.Png);
             
             /*var pca = new PrincipalComponentAnalysis()
             {    
@@ -152,7 +162,7 @@ namespace DicomDisplayTest
             
         }
         
-        protected static float Map(float s, float a1, float a2, float b1, float b2)
+        protected static double Map(double s, double a1, double a2, double b1, double b2)
             // lånt fra https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
         {
             return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
