1469c4f6616aebfd95219c032a92054105edf965
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-03-25 15:24:54 +0100

    added Exception for incompatible Vector/Matrix dimensions

--- LinearAlgebra/Vector.cs
+++ LinearAlgebra/Vector.cs
@@ -31,15 +31,15 @@ namespace LinearAlgebra
     {
       get
       {
-        return _elements.Count;
+	return _elements.Count;
       }
     }
 
     ///<summary>
     ///Initialises a new vector with one element.
     ///</summary>
-    ///<param name=elem>the element the vector contains</param>
-    ///<param name=transposed>transposed state of the vector, this is optional
+    ///<param name="elem">the element the vector contains</param>
+    ///<param name="transposed">transposed state of the vector, this is optional
     ///and defaults to false</param>
     public Vector(double elem, bool transposed = false)
     {
@@ -50,8 +50,8 @@ namespace LinearAlgebra
     ///<summary>
     ///Initialises a new vector with any amount of elements.
     ///</summary>
-    ///<param name=elems>A list of elements to initialise the vector from</param>
-    ///<param name=transposed>transposed state of the vector, this is optional
+    ///<param name="elems">A list of elements to initialise the vector from</param>
+    ///<param name="transposed">transposed state of the vector, this is optional
     ///and defaults to false</param>
     public Vector(IEnumerable<double> elems, bool transposed = false)
     {
@@ -62,7 +62,7 @@ namespace LinearAlgebra
     ///<summary>
     ///Initialises a new null vector
     ///</summary>
-    ///<param name=transposed>transposed state of the vector, this is optional
+    ///<param name="transposed">transposed state of the vector, this is optional
     ///and defaults to false</param>
     public Vector(bool transposed = false)
     {
@@ -78,5 +78,31 @@ namespace LinearAlgebra
       _transposed = _transposed == false ? true : false;
     }
 
+  }
+
+  public class IncompatibleDimensionsException : Exception
+  {
+    ///<summary>
+    ///construct exception
+    ///</summary>
+    public IncompatibleDimensionsException()
+    {
+    }
+
+    ///<summary>
+    ///construct exception with message
+    ///</summary>
+    ///<param name="message">exception message</param>
+    public IncompatibleDimensionsException(string message) 
+      : base(message)
+    {
+    }
+    ///<summary>
+    ///construct exception with message and inner exception
+    ///</summary>
+    ///<param name="message">exception message</param>
+    ///<param name="inner">inner exception</param>
+    public IncompatibleDimensionsException(string message, Exception inner)
+      : base(message, inner)
   }
 }
