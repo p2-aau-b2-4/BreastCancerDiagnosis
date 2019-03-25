d2c427a46d86eae6dead60a1fb13c87799179aa0
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-03-25 15:50:26 +0100

    added scale and dot functions

--- LinearAlgebra/Vector.cs
+++ LinearAlgebra/Vector.cs
@@ -23,7 +23,8 @@ namespace LinearAlgebra
     ///</summary>
     public List<double> Elements { get; }
     ///<summary>
-    ///Gets the transposed state of the vector
+    ///Gets the transposed state of the vector. This is only relevant when doing
+    ///Matrix-Vector calculations.
     ///</summary>
     public bool Transposed { get; }
 
@@ -76,6 +77,36 @@ namespace LinearAlgebra
     public void Transpose()
     {
       _transposed = _transposed == false ? true : false;
+    }
+
+    public static Vector Scale(Vector vector, double scalar) {
+      List<double> elems = new List<double>();
+      foreach (v in vector)
+        elems.Add(v * scalar);
+      return new Vector(elems, vector.Transposed);
+    }
+
+    public Vector Scale(double scalar)
+    {
+      return DotScalar(this, scalar);
+    }
+
+    public static double Dot(Vector vector1, Vector vector2)
+    {
+      if (vector1.Dimensions != vector2.Dimensions)
+        throw new IncompatibleDimensionsException("Vectors with different dimensions cannot be dotted.");
+
+      double result = 0;
+      for (int i = 0; i < vector1.Dimensions; i++)
+      {
+        result =+ vector1[i] * vector2[i];
+      }
+      return result;
+    }
+
+    public double Dot(Vector vector)
+    {
+      return Dot(this, vector);
     }
 
   }
