93a76fe5fe49fcc65680b9fd13f62b16fa9ffceb
Yogi <markamb2497@gmail.com>
2019-03-27 18:19:51 +0100

    PCA class added. Methods needs to be moved into Matrix.cs

---
+++ LinearAlgebra/PCA.cs
@@ -0,0 +1,21 @@
+namespace LinearAlgebra
+{
+    public class PCA
+    {
+        public PCA()
+        {
+            
+        }
+
+        public Matrix CovarianceMatrix()
+        {
+            
+            return new Matrix();
+        }
+
+        public Matrix UnitEigenvectors()
+        {
+            return new Matrix();
+        }
+    }
+}
