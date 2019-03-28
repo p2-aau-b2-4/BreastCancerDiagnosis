f770559990aff35e0889dbab215b1925971176de
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-03-28 15:17:29 +0100

    started ToString function and fixed a rogue }

--- LinearAlgebra/Matrix.cs
+++ LinearAlgebra/Matrix.cs
@@ -108,17 +108,15 @@ namespace LinearAlgebra
         _IA.Add(NNZ);
       }
     }
-    }
 
-    ///<summary>
-    ///Initialises a new null vector
-    ///</summary>
-    ///<param name="transposed">transposed state of the vector, this is optional
-    ///and defaults to false</param>
-    public Vector(bool transposed = false)
+    public override string ToString()
     {
-      _transposed = transposed;
-      _elements.Add(0);
+      if (Transposed) {
+
+      }
+      else {
+        return new string
+      }
     }
 
     ///<summary> 
@@ -128,36 +126,5 @@ namespace LinearAlgebra
     {
       _transposed = _transposed == false ? true : false;
     }
-
-    public Vector operator *(Vector vector, double scalar) {
-      List<double> elems = new List<double>();
-      foreach (v in vector)
-        elems.Add(v * scalar);
-      return new Vector(elems, vector.Transposed);
-    }
-    /* old version
-       public Vector Scale(double scalar)
-       {
-       return DotScalar(this, scalar);
-       }
-       */
-    public double operator *(Vector vector1, Vector vector2)
-    {
-      if (vector1.Dimensions != vector2.Dimensions)
-        throw new IncompatibleDimensionsException("Vectors with different dimensions cannot be dotted.");
-
-      double result = 0;
-      for (int i = 0; i < vector1.Dimensions; i++)
-      {
-        result =+ vector1[i] * vector2[i];
-      }
-      return result;
-    }
-    /* old version
-       public double Dot(Vector vector)
-       {
-       return Dot(this, vector);
-       }
-       */
   }
 }
