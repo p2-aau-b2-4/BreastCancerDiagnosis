30e585bbefca030850e449465663643470d7dab1
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-03-28 15:17:29 +0100

    started ToString function and fixed a rogue }

--- Program.cs
+++ Program.cs
@@ -16,91 +16,20 @@ using Accord.Statistics;
 using Accord.MachineLearning;
 using Accord.Statistics.Analysis;
 using Accord.Statistics.Models.Regression.Linear;
+using LinearAlgebra;
 
 namespace DicomDisplayTest    
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            /*var img = DicomFile.Open(@"e.dcm");
-            UshortImageInfo imgInfo = img.GetUshortImageInfo();
-            imgInfo.Render("brystDef.png");
-
-            imgInfo.ApplyNoiseFilter(1);
-            imgInfo.Render("brystNoiseFilter.png");
-            
-            Bitmap overlayToAdd = new Bitmap(550,550);
-            for (int x = 200; x < 500; x++)
-            {
-                overlayToAdd.SetPixel(x,200,Color.Red);
-                overlayToAdd.SetPixel(x,400,Color.Red);
-            }
-            for (int y = 200; y < 400; y++)
-            {
-                overlayToAdd.SetPixel(200,y,Color.Red);
-                overlayToAdd.SetPixel(500,y,Color.Red);
-            }
-            imgInfo.AddOverlay(overlayToAdd);*/
-
-            Program2.RunPCA();
-            
-            /*var data = FileReaderMNIST.LoadImagesAndLables(
-                "train-labels-idx1-ubyte.gz",
-                "train-images-idx3-ubyte.gz");
-
-            var t = data.GetEnumerator();
-            byte[,] image = new byte[28,28];
-            
-            ImageMark[] imageArray = new ImageMark[30000];
-            List<byte[,]> imageList = new List<byte[,]>();
-            
-            int i = 0;*/
-            
-            // Let's create an analysis with centering (covariance method)
-            // but no standardization (correlation method) and whitening:
-            
-            
-            /*while (t.MoveNext())
-            {
-                image = t.Current.Image;
-                //imageList.Add(image);
-
-                if (t.Current.Label == 1)
-                {
-                    Bitmap imgBitmap = new Bitmap(28,28);
-            
-                    for (int x = 0; x < imgBitmap.Width; x++)
-                    {
-                        for (int y = 0; y < imgBitmap.Height; y++)
-                        {
-                            int greyColor = image[y,x];
-                            imgBitmap.SetPixel(x, y, Color.FromArgb(greyColor, greyColor, greyColor));
-                        }
-                    }
-
-                    var currentLabel = t.Current.Label;
-                    imgBitmap.Save("mnist1/" + i + "_" + ".png", ImageFormat.Png);
-                    i++;
-                }
-                t.MoveNext();
-            }
-            
-            Console.WriteLine(i);
-            t.Dispose();*/
-
-            //imgInfo.Render("bryst.png");
-        }
-    }
-
-    public class ImageMark
-    {
-        private static int _size;
-        private byte[,] _image = new byte[_size,_size];
-        public ImageMark(byte[,] pixels, int size)
-        {
-            _size = size;
-            _image = pixels;
+        static void Main(string[] args) {
+            double[,] arr = new double[4,4] {
+              {2, 0, 0, 3},
+              {0, 0, 0, 0},
+              {0, 5, 7, 0},
+              {0, 4, 0, 0}
+            };
+            Matrix m = new Matrix(arr);
         }
     }
 }
