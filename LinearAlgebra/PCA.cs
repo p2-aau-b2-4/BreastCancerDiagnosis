7e32894b5a57a28e644ea4effc15c62ca0921af4
Yogi <markamb2497@gmail.com>
2019-04-09 14:07:19 +0200

    Work started on eigenvectors and rowreduceing

--- LinearAlgebra/PCA.cs
+++ LinearAlgebra/PCA.cs
@@ -9,9 +9,8 @@ namespace LinearAlgebra
     {
         public PCA()
         {
-            
         }
-        
+
         public void MeanSubtraction(SparseMatrix matrix)
         {
             int tmp = matrix.RowCount;
@@ -22,43 +21,46 @@ namespace LinearAlgebra
             {
                 for (int y = 0; y < matrix.ColumnCount; y++)
                 {
-                    Console.Write(matrix.Storage[x,y] + " ");
+                    Console.Write(matrix.Storage[x, y] + " ");
                 }
 
                 Console.WriteLine();
             }
+
             Console.WriteLine("\n");
-            
-            for(int i = 0; i < matrix.ColumnCount; i++)
+
+            for (int i = 0; i < matrix.ColumnCount; i++)
             {
                 for (int x = 0; x < tmp; x++)
                 {
-                    sum += matrix.Storage[x,i];
+                    sum += matrix.Storage[x, i];
                 }
+
                 xI = sum / tmp;
                 sum = 0;
-      
+
                 for (int x = 0; x < tmp; x++)
                 {
-                    matrix.Storage[x,i] -= xI;
+                    matrix.Storage[x, i] -= xI;
                 }
 
                 xI = 0;
             }
-            
+
             for (int x = 0; x < matrix.RowCount; x++)
             {
                 for (int y = 0; y < matrix.ColumnCount; y++)
                 {
-                    Console.Write(matrix.Storage[x,y] + " ");
+                    Console.Write(matrix.Storage[x, y] + " ");
                 }
 
                 Console.WriteLine();
             }
+
             Console.WriteLine("\n");
         }
-        
-        public void CombinationsSubset(int numberOfElements, int numberOfDimensions, List<int> subsets) 
+
+        public void CombinationsSubset(int numberOfElements, int numberOfDimensions, List<int> subsets)
         {
             CombinationsCovariance(0, 0, numberOfElements, numberOfDimensions, subsets);
         }
@@ -69,8 +71,10 @@ namespace LinearAlgebra
             {
                 subsets.Add(set);
             }
-            else {
-                for (int i = indexPos; i < n; i++) {
+            else
+            {
+                for (int i = indexPos; i < n; i++)
+                {
                     set = set | (1 << i);
 
                     CombinationsCovariance(set, i + 1, r - 1, n, subsets);
@@ -79,16 +83,17 @@ namespace LinearAlgebra
                 }
             }
         }
-        
+
         public double Covariance(double x, double y, int dim)
         {
-            return (x*y)/(dim-1);
+            return (x * y) / (dim - 1);
         }
-        public void CovarianceMatrix(SparseMatrix sparseMatrix)
+
+        public double[,] CovarianceMatrix(SparseMatrix sparseMatrix)
         {
             MeanSubtraction(sparseMatrix);
-            
-            double[,] cMatrix = new double[sparseMatrix.ColumnCount,sparseMatrix.ColumnCount];
+
+            double[,] cMatrix = new double[sparseMatrix.ColumnCount, sparseMatrix.ColumnCount];
 
             for (int x = 0; x < sparseMatrix.ColumnCount; x++)
             {
@@ -96,7 +101,8 @@ namespace LinearAlgebra
                 {
                     for (int i = 0; i < sparseMatrix.RowCount; i++)
                     {
-                        cMatrix[x, y] += Covariance(sparseMatrix.Storage[i,x],sparseMatrix.Storage[i,y],sparseMatrix.RowCount);
+                        cMatrix[x, y] += Covariance(sparseMatrix.Storage[i, x], sparseMatrix.Storage[i, y],
+                            sparseMatrix.RowCount);
                     }
                 }
             }
@@ -105,19 +111,119 @@ namespace LinearAlgebra
             {
                 for (int x = 0; x < sparseMatrix.ColumnCount; x++)
                 {
-                    Console.Write(Math.Round(cMatrix[x,y],5)  + " ");
+                    Console.Write(Math.Round(cMatrix[x, y], 5) + " ");
                 }
 
                 Console.WriteLine();
             }
-            
-      
+
+            return cMatrix;
         }
 
-        public void UnitEigenvectors()
+        public void ReduceRowEchelonForm(SparseMatrix sparseMatrix)
         {
-      
+            int lead = 0;
+            int rowCount = sparseMatrix.RowCount;
+            int columnCount = sparseMatrix.ColumnCount;
+
+            for (int r = 0; r < rowCount; r++)
+            {
+                if (columnCount <= lead)
+                {
+                    break;
+                }
+
+                int i = r;
+
+                while (sparseMatrix[i,lead] == 0)
+                {
+                    i++;
+                    if (rowCount == i)
+                    {
+                        i = r;
+                        lead++;
+                        if (columnCount == lead)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                SwapRows();
+                if (sparseMatrix[r,lead] != 0)
+                {
+                    //If M[r, lead] is not 0 divide row r by M[r, lead]
+                }
+
+                for (; i >= 0 && i < rowCount; i++)
+                {
+                    if (i != r)
+                    {
+                        //Subtract M[i, lead] multiplied by row r from row i
+                    }
+                }
+
+                lead++;
+            }
         }
-       
+
+        public void SwapRows()
+        {
+            
+        }
+
+        public double Determinant(SparseMatrix sparseMatrix)
+        {
+            return sparseMatrix.Determinant();
+        }
+
+        public void Eigenvectors(SparseMatrix sparseMatrix)
+        {
+            List<double> list = new List<double>();
+            list.Add(1);
+            list.Add(0);
+            
+            List<double> list2 = new List<double>();
+            list2.Add(0);
+            list2.Add(1);
+            
+            SparseVector sV = SparseVector.OfEnumerable(list);
+            SparseVector sV2 = SparseVector.OfEnumerable(list2);
+            
+            List<SparseVector> sVL = new List<SparseVector>();
+            sVL.Add(sV);
+            sVL.Add(sV2);
+            
+            /*SparseMatrix sMa = SparseMatrix.OfColumns(sVL);
+
+            sparseMatrix * sMa;
+            
+            Determinant();*/
+        }
+
+        public void UnitEigenvectors(double[,] cArr)
+        {
+            double len = 0.0, len2 = 0.0;
+
+            len = Math.Sqrt((cArr[0, 0] * cArr[0, 0]) + (cArr[1, 0] * cArr[1, 0]));
+            len2 = Math.Sqrt((cArr[0, 1] * cArr[0, 1]) + (cArr[1, 1] * cArr[1, 1]));
+
+            cArr[0, 0] = cArr[0, 0] / len;
+            cArr[1, 0] = cArr[1, 0] / len;
+
+            cArr[0, 1] = cArr[0, 1] / len2;
+            cArr[1, 1] = cArr[1, 1] / len2;
+
+            Console.WriteLine();
+            for (int y = 0; y < 2; y++)
+            {
+                for (int x = 0; x < 2; x++)
+                {
+                    Console.Write(Math.Round(cArr[x, y], 5) + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
