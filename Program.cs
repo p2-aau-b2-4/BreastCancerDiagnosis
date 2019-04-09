a7e2b8e106538ec5a01f10663d319a5a33efb77a
LauJosefsen <ljosef18@student.aau.dk>
2019-04-09 12:37:33 +0200

    Fixede problemer med at finde rigtig mask/crop på en crude mode ved at sammenligne filstørrelser

--- Program.cs
+++ Program.cs
@@ -18,18 +18,53 @@ namespace DicomDisplayTest
     {
         static void Main(string[] args)
         {
-            List<DdsmImage> DDSMImages =
+            List<DdsmImage> DDSMImages = 
                 DdsmImage.GetAllImagesFromCsvFile(@"E:\BrystTest\mass_case_description_train_set.csv");
             Console.WriteLine($"Found {DDSMImages.Count}");
             
+//            Serializer.Save("data.bin", DDSMImages);
+//            foreach (DdsmImage ddsmImage in DDSMImages)
+//            {
+//                Console.WriteLine($"{ddsmImage.PatientId} | {ddsmImage.ImageView} | {ddsmImage.BreastSide} | {ddsmImage.GetDcomCroppedImage().Width}x{ddsmImage.GetDcomCroppedImage().Height}");
+//            }
+
             // Lets render a picture:
-            Console.WriteLine(DDSMImages.First().DcomMaskFilePath);
-            
-            DDSMImages.First().GetDcomOriginalImage().SaveAsPng("original.png");
-            DDSMImages.First().GetDcomMaskImage().SaveAsPng("mask.png");
-            DDSMImages.First().GetDcomCroppedImage().SaveAsPng("cropped.png");
-            
-            
+//            Console.WriteLine(DDSMImages[10].DcomMaskFilePath);
+
+            //DDSMImages.First().GetDcomOriginalImage().SaveAsPng("original.png");
+            //DDSMImages.First().GetDcomMaskImage().SaveAsPng("mask.png");
+            //DDSMImages.First().GetDcomCroppedImage().SaveAsPng("cropped.png");
+
+//            var xImg = DDSMImages.First().GetDcomOriginalImage();
+//            var yImg = DDSMImages.First().GetDcomMaskImage();
+//
+//            Console.WriteLine(DDSMImages.First().Pathology);
+//            var bitmap = new Bitmap(yImg.Width, yImg.Height);
+//            for (int x = 0; x < yImg.Width; x++)
+//            {
+//                for (int y = 0; y < yImg.Height; y++)
+//                {
+//                    byte value = yImg.PixelArray[x, y];
+//                    bool difference = false;
+//                    for (int xin = x - 1; xin < x + 1; xin++)
+//                    {
+//                        if (xin < 0 || xin >= yImg.Width) continue;
+//                        for (int yin = y - 1; yin < y + 1; yin++)
+//                        {
+//                            if (yin < 0 || yin >= yImg.Height) continue;
+//                            if (yImg.PixelArray[xin, yin] != value) difference = true;
+//                        }
+//                    }
+//
+//                    Color color = Color.FromArgb(difference ? 255 : 0, 255, 0, 0);
+//                    bitmap.SetPixel(x, y, color);
+//                }
+//            }
+//
+//            xImg.AddOverlay(bitmap);
+//            xImg.SaveAsPng("test.png");
+
+
             /*
             var img = DicomFile.Open(@"e.dcm");
             UshortImageInfo imgInfo = img.GetUshortImageInfo();
