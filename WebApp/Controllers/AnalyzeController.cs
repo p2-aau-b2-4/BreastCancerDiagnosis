e5f96579be8122a1f65668248a36b7b7e2f90ab6
LauJosefsen <ljosef18@student.aau.dk>
2019-04-16 16:18:41 +0200

    Udklippet markeret af radiologen, foretages nu. Klar til at analyseres - frontend klar (kan finpudses)

--- WebApp/Controllers/AnalyzeController.cs
+++ WebApp/Controllers/AnalyzeController.cs
@@ -29,14 +29,30 @@ namespace WebApp.Controllers
 
         public IActionResult StartAnalyzing()
         {
-            (int, int, int, int) Rectangle = (int.Parse(Request.Form["x1"]),int.Parse(Request.Form["y1"]),int.Parse(Request.Form["x2"]),int.Parse(Request.Form["y2"]));
+            
 
             return Redirect($"~/analyze/showResult/{Request.Form["filePath"]}");
-            return Ok(new{x1 = Rectangle.Item1,y1=Rectangle.Item2,x2=Rectangle.Item3,y2=Rectangle.Item4, file=Request.Form["filePath"]});
+            //return Ok(new{x1 = Rectangle.Item1,y1=Rectangle.Item2,x2=Rectangle.Item3,y2=Rectangle.Item4, file=Request.Form["filePath"]});
         }
 
         public IActionResult ShowAnalysis(String path)
         {
+            (int, int, int, int) Rectangle = (int.Parse(Request.Form["x1"]),int.Parse(Request.Form["y1"]),int.Parse(Request.Form["x2"]),int.Parse(Request.Form["y2"]));
+            path = "/tmp/" + Request.Form["filePath"] + ".tmp";
+            UshortArrayAsImage image = DicomFile.Open(path).GetUshortImageInfo().Crop(Rectangle);
+            
+            // lets first apply the crop
+            
+            // then apply the algorithm
+            
+            //values that should be present:
+            // 1. Image PCA analyzed
+            // 2. Classification and probability
+
+            ViewBag.Classification = DdsmImage.Pathologies.Benign;
+            ViewBag.Probability = 99.5;
+            ViewBag.PcaInputPath = $"https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2018/5bc47b1d196b0.jpg";
+            
             return View();
         }
     }
