38090d449bcdb14143a2bb68e23b7c1dd42fee09
LarsNyborgPedersen <larsnyborgpedersen@gmail.com>
2019-04-17 14:09:27 +0200

    made some tests

---
+++ ImagePreprocessing.Tests/DDSMImageTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ImagePreprocessing.Tests
+{
+    [TestFixture]
+    class DDSMImageTest
+    {
+        [TestCase]
+        public void GetAllImagesFromCsvFile()
+        {
+            List<DdsmImage> DDSMImages =
+                DdsmImage.GetAllImagesFromCsvFile(@"C:\Bryster\mass_case_description_test_set.csv");
+
+            string filePath = DDSMImages.First().DcomFilePath;
+            string testValueResult = @"C:\Bryster\Mass-Test_P_00016_LEFT_CC\10-04-2016-DDSM-30104\1-full mammogram images-14172\000000.dcm";
+
+            Assert.AreEqual(testValueResult, filePath);
+        }
+
+
+
+    }
+}
+
