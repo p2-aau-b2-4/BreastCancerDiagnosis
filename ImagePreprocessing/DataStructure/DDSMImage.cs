116387d6675d3c42181be55297e8aa6fceea70aa
LauJosefsen <ljosef18@student.aau.dk>
2019-04-30 11:17:17 +0200

    JAKOB DAMMIT (og måske lars, idk)

--- ImagePreprocessing/DataStructure/DDSMImage.cs
+++ ImagePreprocessing/DataStructure/DDSMImage.cs
@@ -129,7 +129,6 @@ namespace ImagePreprocessing
 
             if (foldersInFirstFolder.Length > 1)
             {
-                Console.WriteLine("{1}", foldersInFirstFolder);
                 // step 2
                 // lets first find the mask image:
                 foreach (String folderInFirstFolder in foldersInFirstFolder)
