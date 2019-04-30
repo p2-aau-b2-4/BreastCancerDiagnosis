8d5d5f0e6f56b3aa68533f047dfad6f7e8c84e89
JakobLund <45285957+JakobLund@users.noreply.github.com>
2019-04-30 09:00:15 +0200

    Updated DDSMImageTest so that it includes all code paths (if else statement)

--- ImagePreprocessing.Tests/DDSMImageTest.cs
+++ ImagePreprocessing.Tests/DDSMImageTest.cs
@@ -57,12 +57,28 @@ namespace ImagePreprocessing.Tests
             testValueResult = @"D:\Bryster\Mass-Test_P_00016_LEFT_CC_1\10-04-2016-DDSM-09887\1-cropped images-26184\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
+        
+        [TestCase] //Here we have a path with two folders, so it activates the if else statements
+        public void GetMaskImages2()
+        {
+            filePath = DDSMImages.ElementAt(24).DcomMaskFilePath;
+            testValueResult = @"D:\Bryster\Mass-Test_P_00145_LEFT_CC_1\09-27-2017-DDSM-31267\1-ROI mask images-45741\000000.dcm";
+            Assert.AreEqual(testValueResult, filePath);
+        }
 
         [TestCase]
         public void GetCroppedImages()
         {
             filePath = DDSMImages.ElementAt(3).DcomCroppedFilePath;
             testValueResult = @"D:\Bryster\Mass-Test_P_00017_LEFT_MLO_1\10-04-2016-DDSM-27297\1-ROI mask images-18984\000000.dcm";
+            Assert.AreEqual(testValueResult, filePath);
+        }
+
+        [TestCase]  //Here we have a path with two folders, so it activates the if else statements
+        public void GetCroppedImages2()
+        {
+            filePath = DDSMImages.ElementAt(24).DcomCroppedFilePath;
+            testValueResult = @"D:\Bryster\Mass-Test_P_00145_LEFT_CC_1\10-04-2016-DDSM-90238\1-cropped images-53302\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
 
