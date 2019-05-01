1eaeb1d343718cee455be79e13ae85dd6bad2970
LauJosefsen <ljosef18@student.aau.dk>
2019-05-01 18:53:40 +0200

    Trying to use memroycache

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -26,7 +26,7 @@ namespace WebApp
             //    options.CheckConsentNeeded = context => true;
             //    options.MinimumSameSitePolicy = SameSiteMode.None;
             //});
-
+            services.AddMemoryCache();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
