e70c8f3dc844988d135b538610d1cdc5f41755f0
JakobLund <45285957+JakobLund@users.noreply.github.com>
2019-05-03 13:14:48 +0200

    Made SaveAsPNGTest and removed uselss "using"

--- ImagePreprocessing.Tests/ContrastUShortArrayExtensionTest.cs
+++ ImagePreprocessing.Tests/ContrastUShortArrayExtensionTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using Dicom;
+﻿using NUnit.Framework;
 
 namespace ImagePreprocessing.Tests
 {
