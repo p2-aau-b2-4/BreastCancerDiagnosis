3ccddd677b9a0afec0e3b602f5a693b6d967a585
JakobLund <45285957+JakobLund@users.noreply.github.com>
2019-05-10 12:43:25 +0200

    removed global variables because laui didnt like it

--- ImagePreprocessing.Tests/DDSMImageTest.cs
+++ ImagePreprocessing.Tests/DDSMImageTest.cs
@@ -9,8 +9,6 @@ namespace ImagePreprocessing.Tests
     {
         List<DdsmImage> DDSMImages;
 
-        string filePath;
-        string testValueResult;
 
         [SetUp]
         public void Setup()
@@ -24,8 +22,8 @@ namespace ImagePreprocessing.Tests
         public void GetAllImagesFromCsvFileTest1()
         {
 
-            filePath = DDSMImages.ElementAt(0).DcomFilePath;
-            testValueResult = @"D:\Bryster\Mass-Test_P_00016_LEFT_CC\10-04-2016-DDSM-30104\1-full mammogram images-14172\000000.dcm";
+            string filePath = DDSMImages.ElementAt(0).DcomFilePath;
+            string testValueResult = @"D:\Bryster\Mass-Test_P_00016_LEFT_CC\10-04-2016-DDSM-30104\1-full mammogram images-14172\000000.dcm";
 
             Assert.AreEqual(testValueResult, filePath);
 
@@ -34,48 +32,48 @@ namespace ImagePreprocessing.Tests
         [TestCase]
         public void GetAllImagesFromCsvFileTest2()
         {
-            filePath = DDSMImages.ElementAt(1).DcomFilePath;
-            testValueResult = @"D:\Bryster\Mass-Test_P_00016_LEFT_MLO\10-04-2016-DDSM-54392\1-full mammogram images-35518\000000.dcm";
+            string filePath = DDSMImages.ElementAt(1).DcomFilePath;
+            string testValueResult = @"D:\Bryster\Mass-Test_P_00016_LEFT_MLO\10-04-2016-DDSM-54392\1-full mammogram images-35518\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
 
         [TestCase]
         public void GetAllImagesFromCsvFileTest3()
         {
-            filePath = DDSMImages.ElementAt(2).DcomFilePath;
-            testValueResult = @"D:\Bryster\Mass-Test_P_00017_LEFT_CC\10-04-2016-DDSM-12683\1-full mammogram images-82967\000000.dcm";
+            string filePath = DDSMImages.ElementAt(2).DcomFilePath;
+            string testValueResult = @"D:\Bryster\Mass-Test_P_00017_LEFT_CC\10-04-2016-DDSM-12683\1-full mammogram images-82967\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
 
         [TestCase]
         public void GetMaskImages()
         {
-            filePath = DDSMImages.ElementAt(0).DcomMaskFilePath;
-            testValueResult = @"D:\Bryster\Mass-Test_P_00016_LEFT_CC_1\10-04-2016-DDSM-09887\1-cropped images-26184\000000.dcm";
+            string filePath = DDSMImages.ElementAt(0).DcomMaskFilePath;
+            string testValueResult = @"D:\Bryster\Mass-Test_P_00016_LEFT_CC_1\10-04-2016-DDSM-09887\1-cropped images-26184\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
         
         [TestCase] //Here we have a path with two folders, so it activates the if else statements
         public void GetMaskImages2()
         {
-            filePath = DDSMImages.ElementAt(24).DcomMaskFilePath;
-            testValueResult = @"D:\Bryster\Mass-Test_P_00145_LEFT_CC_1\09-27-2017-DDSM-31267\1-ROI mask images-45741\000000.dcm";
+            string filePath = DDSMImages.ElementAt(24).DcomMaskFilePath;
+            string testValueResult = @"D:\Bryster\Mass-Test_P_00145_LEFT_CC_1\09-27-2017-DDSM-31267\1-ROI mask images-45741\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
 
         [TestCase]
         public void GetCroppedImages()
         {
-            filePath = DDSMImages.ElementAt(3).DcomCroppedFilePath;
-            testValueResult = @"D:\Bryster\Mass-Test_P_00017_LEFT_MLO_1\10-04-2016-DDSM-27297\1-ROI mask images-18984\000000.dcm";
+            string filePath = DDSMImages.ElementAt(3).DcomCroppedFilePath;
+            string testValueResult = @"D:\Bryster\Mass-Test_P_00017_LEFT_MLO_1\10-04-2016-DDSM-27297\1-ROI mask images-18984\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
 
         [TestCase]  //Here we have a path with two folders, so it activates the if else statements
         public void GetCroppedImages2()
         {
-            filePath = DDSMImages.ElementAt(24).DcomCroppedFilePath;
-            testValueResult = @"D:\Bryster\Mass-Test_P_00145_LEFT_CC_1\10-04-2016-DDSM-90238\1-cropped images-53302\000000.dcm";
+            string filePath = DDSMImages.ElementAt(24).DcomCroppedFilePath;
+            string testValueResult = @"D:\Bryster\Mass-Test_P_00145_LEFT_CC_1\10-04-2016-DDSM-90238\1-cropped images-53302\000000.dcm";
             Assert.AreEqual(testValueResult, filePath);
         }
 
