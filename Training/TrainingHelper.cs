b864e29d1aa28a0b6be8e86ecd476305b555556a
Oliversrensen <ossa18@student.aau.dk>
2019-05-20 16:13:27 +0200

    Done a lot of cleaning

--- Training/TrainingHelper.cs
+++ Training/TrainingHelper.cs
@@ -41,107 +41,16 @@ namespace Training
         /// <returns>A new parameter set, only C and gamma will have been optimized</returns>
         public static SVMParameter FindBestHyperparameters(SVMProblem problem, SVMParameter parameter)
         {
-//            ParameterRange parameterCoarseRange =
-//                FindOptimalRange(new ParameterRange() {FromC = Math.Pow(0.001, FromGamma = 0.01, ToC = 100, ToGamma = 100},
-//                    (x) => (Math.Pow(x, 2)), (x) => (Math.Log(x,2)), problem, parameter);
-
-            ParameterRange parameterFineRange = FindOptimalRangeUsingLog(new ParameterRange(), (x) => Math.Pow(x, 2),
+            ParameterRange parameterFineRange = FindOptimalRangeUsingLog((x) => Math.Pow(x, 2),
                 (x) => (Math.Log(x,2)), problem, parameter, true);
+            
             parameter.C = parameterFineRange.FromC;
             parameter.Gamma = parameterFineRange.FromGamma;
+            
             return parameter;
         }
 
-        private static ParameterRange FindOptimalRange(ParameterRange parameterRange, Func<double, double> func,
-            Func<double, double> revFunc,
-            SVMProblem problem, SVMParameter parameter, bool returnFromValuesAsAnswer = false)
-        {
-            int nFold = int.Parse(Configuration.Get("nFold"));
-
-            BlockingCollection<ParameterResult> results = new BlockingCollection<ParameterResult>();
-            List<Task> tasks = new List<Task>();
-            for (double cIn = parameterRange.FromC; cIn <= parameterRange.ToC; cIn = func(cIn))
-            {
-                double c = cIn.DeepClone();
-                tasks.Add(Task.Factory.StartNew(() =>
-                {
-                    for (double gamma = parameterRange.FromGamma; gamma <= parameterRange.ToGamma; gamma = func(gamma))
-                    {
-                        SVMParameter parameterUnderTest = parameter.Clone();
-                        parameterUnderTest.C = c;
-                        parameterUnderTest.Gamma = gamma;
-                        problem.CrossValidation(parameterUnderTest, nFold, out var crossValidationResults);
-                        double crossValidationAccuracy = problem.EvaluateClassificationProblem(crossValidationResults);
-
-                        results.Add(new ParameterResult()
-                        {
-                            Accuracy = crossValidationAccuracy, C = parameterUnderTest.C,
-                            Gamma = parameterUnderTest.Gamma
-                        });
-                    }
-                }));
-            }
-
-            Task.WaitAll(tasks.ToArray());
-            
-            //find the highest score:
-            double highestScore = 0;
-            SVMParameter bestParameter = parameter.Clone();
-            foreach (ParameterResult result in results.ToList())
-            {
-                if (result.Accuracy > highestScore)
-                {
-                    highestScore = result.Accuracy;
-                    bestParameter.C = result.C;
-                    bestParameter.Gamma = result.Gamma;
-                }
-            }
-
-            if (bestParameter == null) throw new Exception("Something went really wrong, no parameters was found.");
-
-            ParameterRange fineParameterRange = new ParameterRange();
-            if (!returnFromValuesAsAnswer)
-            {
-                fineParameterRange.ToC = func(bestParameter.C);
-                fineParameterRange.FromC = revFunc(bestParameter.C);
-                fineParameterRange.ToGamma = func(bestParameter.Gamma);
-                fineParameterRange.FromGamma = revFunc(bestParameter.Gamma);
-            }
-            else
-            {
-                fineParameterRange.FromC = bestParameter.C;
-                fineParameterRange.FromGamma = bestParameter.Gamma;
-            }
-
-            // lets save the dictionary to a csv file:
-            // loop through every c value and create a header, then for every gamma value, create a sideheader and insert values:
-            using (StreamWriter file = new StreamWriter(@"svmData.txt", true))
-            {
-                //createheader:
-                file.Write($@"{"C\\G",5}"); // empty topleft corner
-                for (double gamma = parameterRange.FromGamma; gamma <= parameterRange.ToGamma; gamma = func(gamma))
-                {
-                    file.Write($"{gamma,5},");
-                }
-
-                file.WriteLine();
-
-                for (double c = parameterRange.FromC; c <= parameterRange.ToC; c = func(c))
-                {
-                    file.Write($"{c,5},");
-                    for (double gamma = parameterRange.FromGamma; gamma <= parameterRange.ToGamma; gamma = func(gamma))
-                    {
-                        file.Write($"{Math.Round(results.Where(x => (Math.Abs(x.C - c) < 0.0001 && Math.Abs(x.Gamma - gamma) < 0.0001)).ToArray()[0].Accuracy,2),5},");
-                    }
-
-                    file.WriteLine();
-                }
-            }
-
-            return fineParameterRange;
-        }
-        private static ParameterRange FindOptimalRangeUsingLog(ParameterRange parameterRange, Func<double, double> func,
-            Func<double, double> revFunc,
+        private static ParameterRange FindOptimalRangeUsingLog(Func<double, double> func, Func<double, double> revFunc,
             SVMProblem problem, SVMParameter parameter, bool returnFromValuesAsAnswer = false)
         {
             int nFold = int.Parse(Configuration.Get("nFold"));
@@ -173,7 +82,49 @@ namespace Training
             }
 
             Task.WaitAll(tasks.ToArray());
+
+            ParameterRange fineParameterRange =
+                HighestScore(results, func, revFunc, parameter, returnFromValuesAsAnswer);
             
+            SaveToCSV(results);
+
+            return fineParameterRange;
+        }
+
+        private static void SaveToCSV(BlockingCollection<ParameterResult> results)
+        {
+            int logTo = int.Parse(Configuration.Get("logTo"));
+            int logFrom = int.Parse(Configuration.Get("logFrom"));
+            
+            // lets save the dictionary to a csv file:
+            // loop through every c value and create a header, then for every gamma value, create a sideheader and insert values:
+            using (StreamWriter file = new StreamWriter(@"svmData.txt", true))
+            {
+                //createheader:
+                file.Write($@"{"C\\G",5}"); // empty topleft corner
+                for (double gammaLog = logFrom; gammaLog <= logTo; gammaLog++)
+                {
+                    file.Write($"{Math.Pow(2,gammaLog),5},");
+                }
+
+                file.WriteLine();
+
+                for (double cLog = logFrom; cLog <= logTo; cLog++)
+                {
+                    file.Write($"{Math.Pow(2,cLog),5},");
+                    for (double gammaLog = logFrom; gammaLog <= logTo; gammaLog++)
+                    {
+                        file.Write($"{Math.Round(results.Where(x => (Math.Abs(x.C - Math.Pow(2,cLog)) < 0.0001 && Math.Abs(x.Gamma - Math.Pow(2,gammaLog)) < 0.0001)).ToArray()[0].Accuracy,2),5},");
+                    }
+
+                    file.WriteLine();
+                }
+            }
+        }
+
+        private static ParameterRange HighestScore(BlockingCollection<ParameterResult> results, Func<double, double> func,
+                      Func<double, double> revFunc, SVMParameter parameter, bool returnFromValuesAsAnswer = false)
+        {
             //find the highest score:
             double highestScore = 0;
             SVMParameter bestParameter = parameter.Clone();
@@ -202,36 +153,9 @@ namespace Training
                 fineParameterRange.FromC = bestParameter.C;
                 fineParameterRange.FromGamma = bestParameter.Gamma;
             }
-
-            // lets save the dictionary to a csv file:
-            // loop through every c value and create a header, then for every gamma value, create a sideheader and insert values:
-            using (StreamWriter file = new StreamWriter(@"svmData.txt", true))
-            {
-                //createheader:
-                file.Write($@"{"C\\G",5}"); // empty topleft corner
-                for (double gammaLog = logFrom; gammaLog <= logTo; gammaLog++)
-                {
-                    file.Write($"{Math.Pow(2,gammaLog),5},");
-                }
-
-                file.WriteLine();
-
-                for (double cLog = logFrom; cLog <= logTo; cLog++)
-                {
-                    file.Write($"{Math.Pow(2,cLog),5},");
-                    for (double gammaLog = logFrom; gammaLog <= logTo; gammaLog++)
-                    {
-                        file.Write($"{Math.Round(results.Where(x => (Math.Abs(x.C - Math.Pow(2,cLog)) < 0.0001 && Math.Abs(x.Gamma - Math.Pow(2,gammaLog)) < 0.0001)).ToArray()[0].Accuracy,2),5},");
-                    }
-
-                    file.WriteLine();
-                }
-            }
-
+            
             return fineParameterRange;
         }
-        
-        
 
         public static PCA GetPca(List<ImageWithResultModel> images)
         {
