8d4cde304e4b128ee1c63b1cb85d9f7c47f3bcc5
Lasse Oliver Nicklas Jensen <lonj18@student.aau.dk>
2019-05-21 12:43:51 +0200

    Merge remote-tracking branch 'origin/OliverLasse' into OliverLasse
    # Conflicts:
    #	ImagePreprocessing.Tests/NormalizationTest.cs
    #	ImagePreprocessing/Program.cs

--- Training.Tests/TrainingHelperTests.cs
+++ Training.Tests/TrainingHelperTests.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Concurrent;
 using CsvHelper.Configuration;
 using NUnit.Framework;
+using System.Linq;
+using LibSVMsharp;
 
 namespace Training.Tests
 {
@@ -14,12 +17,12 @@ namespace Training.Tests
         }
 
         [TestCase]
-        public void SaveToCSVTest()
+        public void HighestScoreTest()
         {
+            // Setup results
             BlockingCollection<TrainingHelper.ParameterResult> results =
                 new BlockingCollection<TrainingHelper.ParameterResult>();
-
-            int nFold = 5;
+            
             int logTo = 8;
             int logFrom = -6;
 
@@ -29,14 +32,44 @@ namespace Training.Tests
                 {
                     results.Add(new TrainingHelper.ParameterResult()
                     {
-                        Accuracy = 1, C = 2,
-                        Gamma = 3
+                        Accuracy = 1, C = Math.Pow(2, cLog),
+                        Gamma = Math.Pow(2, gammaLog)
                     });
                 }
-
-                TrainingHelper.SaveToCSV(results);
-                Assert.Pass();
             }
+            
+            //Setup func and revFunc
+            Func<double, double> func = (x) => Math.Pow(x, 2);
+            Func<double, double> revFunc = (x) => (Math.Log(x, 2));
+            
+            //Setup test SVMParameter
+            SVMParameter parameter = new SVMParameter
+            {
+                Type = SVMType.C_SVC,
+                Kernel = SVMKernelType.RBF,
+                C = 2,
+                Gamma = 1,
+                Probability = true,
+                WeightLabels = new[] {0, 1},
+                Weights = new[] {(1 - 0.69)/0.69, 1}
+            };
+            
+            // Expected parameterRange
+            var realValue = new TrainingHelper.ParameterRange()
+            {
+                FromC = 0, ToC = 0, FromGamma = 0, ToGamma = 0
+            };
+            
+            // Actual parameterRange
+            var actualValue = TrainingHelper.HighestScore(results, func, revFunc, parameter);
+            
+            Assert.AreEqual(realValue, actualValue);
+
+        }
+
+        [TestCase]
+        public void SaveToCSVTest()
+        {
 
         }
     }
