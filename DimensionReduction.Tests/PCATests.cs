5b335dc015fc21f74c3c6fdfd655b8989535c5ca
Yogi <markamb2497@gmail.com>
2019-05-21 13:52:39 +0200

    Removed unused code

--- DimensionReduction.Tests/PCATests.cs
+++ DimensionReduction.Tests/PCATests.cs
@@ -390,18 +390,6 @@ namespace DimensionReduction.Tests
         public void TrainNumberOfComponents() // Not working
         {
             PCA p = new PCA();
-            double[,] matrix = new double[10,2] {
-                {0.69, 0.49},
-                {-1.31, -1.21},
-                {0.39, 0.99},
-                {0.09, 0.29},
-                {1.29, 1.09},
-                {0.49, 0.79},
-                {0.19, -0.31},
-                {-0.81, -0.81},
-                {-0.31, -0.31},
-                {-0.71, -1.01}
-            };
             
             double[,] matrix2 = new double[10,2] {
                 {1.507, 0.988},
@@ -439,19 +427,6 @@ namespace DimensionReduction.Tests
                 {-0.71, -1.01}
             };
             
-            double[,] matrix2 = new double[10,2] {
-                {1.507, 0.988},
-                {2.107, -9.312},
-                {1.407, 1.798},
-                {1.397, 2.098},
-                {-9.563, 1.988},
-                {0.797, 0.888},
-                {2.607, 0.488},
-                {-0.493, 1.588},
-                {0.627, -0.412},
-                {-0.393, -0.112}
-            };
-
             double[] expectation = {0.6778734, 0.73517866}; // Verified with octave
             
             p.Train(matrix);
@@ -476,20 +451,6 @@ namespace DimensionReduction.Tests
             matrix[8] = new double[] {-0.31, -0.31};
             matrix[9] = new double[] {-0.71, -1.01};
             
-            
-            double[,] matrix2 = new double[10,2] {
-                {1.507, 0.988},
-                {2.107, -9.312},
-                {1.407, 1.798},
-                {1.397, 2.098},
-                {-9.563, 1.988},
-                {0.797, 0.888},
-                {2.607, 0.488},
-                {-0.493, 1.588},
-                {0.627, -0.412},
-                {-0.393, -0.112}
-            };
-
             double[] expectation = {0.6778734, 0.73517866}; // Verified with octave
             
             p.Train(matrix);
